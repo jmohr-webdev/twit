d9b5ef8a8856d1ff80cef892591fd342f4738be4
J. Mohr <pixels.and.glitches@gmail.com>
2021-04-15 12:49:24 -0500

    Created Twit model

--- models/Twit.js
+++ models/Twit.js
@@ -1,1 +1,15 @@
 const mongoose = require('mongoose');
+
+const TwitSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+    maxLength: 400,
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model('Twit', TwitSchema);
