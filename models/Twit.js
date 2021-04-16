6bd6a02416d53487dbf604f34b6d43f123a4bf88
J. Mohr <jmohr.webdev@gmail.com>
2021-04-16 11:59:38 -0500

    Twit POST route

--- models/Twit.js
+++ models/Twit.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const TwitSchema = new mongoose.Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+  },
   content: {
     type: String,
     required: true,
--- routes/twits.js
+++ routes/twits.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
+const Twit = require('../models/Twit');
+const User = require('../models/User');
 
 router.get('/twits', (req, res) => {
   res.send(`Get all twits by ${req.params.username}`);
@@ -7,6 +9,35 @@ router.get('/twits', (req, res) => {
 
 router.get('/twits/:id', (req, res) => {
   res.send(`${req.params.id} by ${req.params.username}`);
+});
+
+router.post('/twits', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username });
+
+    console.log(user);
+
+    if (!user) {
+      return res.status(400).json({ msg: 'User not found', success: false });
+    }
+
+    const newTwit = new Twit({
+      content: req.body.content,
+      user: user._id,
+    });
+
+    await Twit.create(newTwit);
+
+    res
+      .status(200)
+      .json({ msg: 'New twit created', success: true, data: newTwit });
+  } catch (error) {
+    res.status(500).json({
+      msg: 'Something went wrong',
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
 module.exports = router;
