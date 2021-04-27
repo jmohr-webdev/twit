a728174479dcbb326182b65b4c3dfc81edf6efad
J. Mohr <jmohr.webdev@gmail.com>
2021-04-27 00:30:18 -0500

    Added profile GET, PUT, DELETE routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const colors = require('colors');
 const connectDB = require('./config/db');
 const user = require('./routes/user');
 const twits = require('./routes/twits');
+const profile = require('./routes/profile');
 
 const app = express();
 dotenv.config({ path: './config/config.env' });
@@ -18,7 +19,8 @@ const PORT = process.env.PORT || 5000;
 
 // Routes
 app.use('/api/v1/', user);
-app.use('/api/v1/:username/', twits);
+app.use('/api/v1/:username/', profile);
+app.use('/api/v1/:username/twits/', twits);
 
 app.listen(PORT, () => {
   console.log(`Now listening on port ${PORT}`.rainbow.bgBlack);
