3008e0fdc00a66483d39461b140e85c548e4be18
J. Mohr <pixels.and.glitches@gmail.com>
2021-04-28 16:02:07 -0500

    Made a separate auth middleware for when user only needs to be logged in

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const colors = require('colors');
 
 const connectDB = async () => {
   try {
+    mongoose.set('useCreateIndex', true);
     const db = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
